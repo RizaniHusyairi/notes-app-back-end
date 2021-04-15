0bdcb2d791cffe566853897148005a8130acc682
Rizani husyairi <leo.rizan68@gmail.com>
2021-04-15 20:14:37 +0800

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+/* eslint-disable linebreak-style */
+
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require("./handler");
+
+// eslint-disable-next-line no-unused-vars
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    
+  },
+  {
+      method: 'GET',
+      path: '/notes',
+      handler:getAllNotesHandler,
+  },
+  {
+      method: 'GET',
+      path: '/notes/{id}',
+      handler:getNoteByIdHandler,
+  },
+  {
+      method:'PUT',
+      path: '/notes/{id}',
+      handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+
+];
+
+module.exports = routes;
